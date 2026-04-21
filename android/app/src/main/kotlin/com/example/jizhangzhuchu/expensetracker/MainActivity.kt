3bf930dcbe9e8caf6109e4979a1ebd68dc89b0dc
dong <madong@zqykj.com>
2026-04-21 14:49:47 +0800

    空项目

---
+++ android/app/src/main/kotlin/com/example/jizhangzhuchu/expensetracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jizhangzhuchu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  testWidgets('loads home page, switches tabs, and navigates to add record page', (WidgetTester tester) async {
+    
+  });
+}
